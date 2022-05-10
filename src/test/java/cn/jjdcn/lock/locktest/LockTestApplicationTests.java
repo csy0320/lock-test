228d47095380cc5a37b415845fda45890331cdbb
caisy <shzu_csy@163.com>
2022-05-10 20:12:29 +0800

    first commit

---
+++ src/test/java/cn/jjdcn/lock/locktest/LockTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jjdcn.lock.locktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
