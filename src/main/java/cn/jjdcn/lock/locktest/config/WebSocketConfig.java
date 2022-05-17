3024cdb34352a16c5142d50eaf9d34978ce03763
caisy <shzu_csy@163.com>
2022-05-17 20:45:00 +0800

    feat: web socket demo

---
+++ src/main/java/cn/jjdcn/lock/locktest/config/WebSocketConfig.java
@@ -0,0 +1,15 @@
+package cn.jjdcn.lock.locktest.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
